af092004924dc0b8e64c8f5b5de332881e14f3e9
yinon <yinon.bentarya@gmail.com>
2019-05-03 16:48:43 +0300

    routs links

--- routingEx/src/app/app.component.ts
+++ routingEx/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routingEx';
+  isLoading = false;
+  redirctTo(url: string) {
+    this.router.navigate(['../', url], { relativeTo: this.activetedRoute });
+  }
+
+  constructor(private router: Router, private activetedRoute: ActivatedRoute) {
+    router.events.pipe(filter(x => x instanceof NavigationStart)).subscribe(x => {
+      this.isLoading = true;
+      console.log('NavigationStart');
+    }
+    );
+    router.events.pipe(filter(x => x instanceof NavigationEnd)).subscribe(x => {
+      this.isLoading = false;
+      console.log('NavigationEnd');
+
+    });
+
+  }
 }
